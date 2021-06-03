855d477b9597c64913e60fe9b821a651d9fc60f0
Darshit Trevadia <darshithtrevadia@gmail.com>
2021-06-03 19:14:17 +0530

    Edit config.py to rearrange data_base

--- data/config.py
+++ data/config.py
@@ -105,28 +105,6 @@ class Config(object):
 
 # ----------------------- DATASETS ----------------------- #
 
-cityscapes_dataset = dataset_base.copy({
-    'name': 'CityScape Custom Dataset',
-    'train_info': '/content/gdrive/MyDrive/instancesonly_filtered_gtFine_train.json',
-    'train_images': '/content/gdrive/MyDrive/Internship/gtFine/train/',
-    'valid_info': '/content/gdrive/MyDrive/instancesonly_filtered_gtFine_val.json',
-    'valid_images': '/content/gdrive/MyDrive/Internship/gtFine/val/',
-    'class_names':('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
-                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
-                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
-                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
-                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-                'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
-                'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
-                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
-                'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
-                'scissors', 'teddy bear', 'hair drier', 'toothbrush'),
-    'label_map': {1: 1}
-})
 
 dataset_base = Config({
     'name': 'Base Dataset',
@@ -195,6 +173,28 @@ pascal_sbd_dataset = dataset_base.copy({
     'class_names': PASCAL_CLASSES,
 })
 
+cityscapes_dataset = dataset_base.copy({
+    'name': 'CityScape Custom Dataset',
+    'train_info': '/content/gdrive/MyDrive/instancesonly_filtered_gtFine_train.json',
+    'train_images': '/content/gdrive/MyDrive/Internship/gtFine/train/',
+    'valid_info': '/content/gdrive/MyDrive/instancesonly_filtered_gtFine_val.json',
+    'valid_images': '/content/gdrive/MyDrive/Internship/gtFine/val/',
+    'class_names':('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+                'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+                'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
+                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+                'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+                'scissors', 'teddy bear', 'hair drier', 'toothbrush'),
+    'label_map': {1: 1}
+})
 
 
 
